75c8cead341a7f1af3b55f2131be34326f89bb80
dhoumaima <dhoumaima077@gmail.com>
2024-07-30 09:47:09 +0100

    display structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
 function App() {
   return (
     <div className="App">
-    <h1>Expense Tracker</h1>
+      <Header/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses/>
+      </div>
     </div>
   );
 }
